8a8ce3a8db99a64aa57705cc540e804cbbcd59f5
Axelrom16 <romeroaxel16@gmail.com>
2023-12-04 12:28:23 +0100

    Modification of dataset

--- source/data.py
+++ source/data.py
@@ -9,67 +9,24 @@ from torch.utils.data import Dataset
 from PIL import Image
 from torchvision import transforms
 import matplotlib.pyplot as plt
+import albumentations
+import cv2
 
-
-class RAVIRDataset(Dataset):
-    """
-    Dataset class for the RAVIR dataset
-    """
-    def __init__(self,
-                 root_dir,
-                 train=True):
-        """
-        Args:
-            root_dir (string): Directory with all the images.
-            transform (callable, optional): Optional transform to be applied on a sample.
-        """
-        self.root_dir = root_dir
-        self.train = train
-
-        if self.train:
-            self.images = os.listdir(os.path.join(self.root_dir, 'training_images'))
-            self.masks = os.listdir(os.path.join(self.root_dir, 'training_masks'))
-        else:
-            self.images = os.listdir(self.root_dir)
-
-    def __len__(self):
-        return len(self.images)
-
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        img_path = os.path.join(self.root_dir, self.images[idx])
-        image = Image.open(img_path)
-        image = image.convert('RGB')
-
-        mask = None
-        if self.train:
-            mask_path = os.path.join(self.root_dir, self.masks[idx])
-            mask = Image.open(mask_path)
-
-        return image, mask
     
-class SegmentationBase(Dataset):
+class RAVIRDataset(Dataset):
     def __init__(self,
-                 data_csv, data_root, segmentation_root,
+                 data_root, segmentation_root,
                  size=None, random_crop=False, interpolation="bicubic",
                  n_labels=1, shift_segmentation=False,
                  augmentation=False,
                  ):
         self.n_labels = n_labels
         self.shift_segmentation = shift_segmentation
-        self.data_csv = data_csv
         self.data_root = data_root
         self.segmentation_root = segmentation_root
         
-        self.image_ids = []
-        self.gleason_list = []
-        with open(os.path.join(self.data_root, self.data_csv), "r") as f:
-            lines = f.read().splitlines()
-            for line in lines[1:]:
-                self.image_ids.append(line.split(',')[1])
-                self.gleason_list.append(line.split(',')[2])
+        self.image_ids = os.listdir(self.data_root)
+        self.image_ids = [l.split('.')[0] for l in self.image_ids]
              
         self._length = len(self.image_ids)
         self.labels = {
@@ -77,9 +34,8 @@ class SegmentationBase(Dataset):
             "relative_file_path_": [l+'.png' for l in self.image_ids],
             "file_path_": [os.path.join(self.data_root, l+'.png')
                            for l in self.image_ids],
-            "segmentation_path_": [os.path.join(self.segmentation_root, l+'_mask.png')
-                                   for l in self.image_ids],
-            "gleason": self.gleason_list
+            "segmentation_path_": [os.path.join(self.segmentation_root, l+'.png')
+                                   for l in self.image_ids]
         }
 
         size = None if size is not None and size<=0 else size
@@ -125,6 +81,7 @@ class SegmentationBase(Dataset):
 
         segmentation = Image.open(example["segmentation_path_"])
         segmentation = np.array(segmentation).astype(np.uint8)
+        segmentation = ((segmentation / 255) * 2).astype(np.uint8)
 
         if self.size is not None:
             image = self.image_rescaler(image=image)["image"]
@@ -146,7 +103,7 @@ class SegmentationBase(Dataset):
         if self.augmentation is not None:
             processed = self.augmentation(image=processed['image'], mask=processed['mask'])       
 
-        example["image"] = (processed["image"]/127.5 - 1.0).astype(np.float32)
+        example["image"] = (processed["image"]/255).astype(np.float32)
 
         example["mask"] = np.expand_dims(processed["mask"], -1)
 
@@ -165,15 +122,21 @@ class SegmentationBase(Dataset):
 
 
 if __name__ == '__main__':
-    data = RAVIRDataset(root_dir='data/train',
-                        train=True)
-    print(len(data))
+    data = RAVIRDataset(
+        data_root='/Volumes/Axel/Master/3rd Semester/DLMIA/RAVIR_Project/data/train/training_images',
+        segmentation_root='/Volumes/Axel/Master/3rd Semester/DLMIA/RAVIR_Project/data/train/training_masks',
+        size=256,
+        random_crop=False,
+        interpolation="bicubic",
+        n_labels=3,
+        shift_segmentation=False,
+        augmentation=True
+    )
 
-    image, mask = data[0]
-    print(image.size)
-    print(mask.size)
+    print(data[0]['image'].shape)
+    print(data[0]['mask'].shape)
 
-    plt.imshow(image)
+    plt.imshow(data[0]['image'])
     plt.show()
-    plt.imshow(mask)
+    plt.imshow(data[0]['mask'], cmap='gray')
     plt.show()
