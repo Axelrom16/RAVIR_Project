6c2cc809108c8532265e20bb9560468a669e5efb
Axelrom16 <romeroaxel16@gmail.com>
2023-12-05 18:17:23 +0100

    Auxiliar stream and full loss

--- source/segravir.py
+++ source/segravir.py
@@ -4,10 +4,7 @@ Main script of the SegRavir Model.
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-from torch.utils.data import DataLoader
-from torchview import draw_graph
-from torchviz import make_dot
-import matplotlib.pyplot as plt
+import segmentation_models_pytorch as smp
 
 
 class ResidualBlock(nn.Module):
@@ -76,7 +73,7 @@ class SegRAVIRModel(nn.Module):
             ResidualBlock(256, 256)
         ]) 
 
-        self.decoder = nn.ModuleList([
+        self.decoder_mask = nn.ModuleList([
             # (Batch_Size, 256, Height / 16, Width / 16) --> (Batch_Size, 128, Height / 8, Width / 8)
             nn.ConvTranspose2d(256, 128, stride=2, kernel_size=3, padding=1, output_padding=1),
             ResidualBlock(128, 128),
@@ -95,10 +92,38 @@ class SegRAVIRModel(nn.Module):
 
             # (Batch_Size, 16, Height, Width) --> (Batch_Size, 16, Height, Width)
             nn.Conv2d(32, 16, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+
             nn.Conv2d(16, 16, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
 
             # (Batch_Size, 16, Height, Width) --> (Batch_Size, 3, Height, Width)
-            nn.Conv2d(16, 3, kernel_size=1)
+            nn.Conv2d(16, 3, kernel_size=1),
+            nn.Softmax(dim=1)
+        ])
+
+        self.decoder_reconstruction = nn.ModuleList([
+            # (Batch_Size, 256, Height / 16, Width / 16) --> (Batch_Size, 128, Height / 8, Width / 8)
+            nn.ConvTranspose2d(256, 128, stride=2, kernel_size=3, padding=1, output_padding=1),
+            ResidualBlock(128, 128),
+
+            # (Batch_Size, 128, Height / 8, Width / 8) --> (Batch_Size, 64, Height / 4, Width / 4)
+            nn.ConvTranspose2d(256, 64, stride=2, kernel_size=3, padding=1, output_padding=1),
+            ResidualBlock(64, 64),
+
+            # (Batch_Size, 64, Height / 4, Width / 4) --> (Batch_Size, 32, Height / 2, Width / 2)
+            nn.ConvTranspose2d(128, 32, stride=2, kernel_size=3, padding=1, output_padding=1),
+            ResidualBlock(32, 32),
+
+            # (Batch_Size, 32, Height / 2, Width / 2) --> (Batch_Size, 16, Height, Width)
+            nn.ConvTranspose2d(64, 16, stride=2, kernel_size=3, padding=1, output_padding=1),
+            ResidualBlock(16, 16),
+
+            # (Batch_Size, 16, Height, Width) --> (Batch_Size, 3, Height, Width)
+            nn.Conv2d(32, 3, kernel_size=1),
+            nn.ReLU()
         ])
 
     def forward(self, x):
@@ -116,39 +141,59 @@ class SegRAVIRModel(nn.Module):
         for layers in self.bottleneck:
             x = layers(x)
 
+        # Copy of the output of the bottleneck
+        mask = x
+        skip_connections_2 = skip_connections.copy()
+
+        # Main decoder stream 
         skip_decoder = [2, 4, 6, 8]
         i = 0
         j = 0
-        for layers in self.decoder:
+        for layers in self.decoder_mask:
             if i in skip_decoder:
-                x = torch.cat((x, skip_connections.pop()), dim=1)
+                mask = torch.cat((mask, skip_connections.pop()), dim=1)
+                j += 1
+            mask = layers(mask)
+            i += 1
+        
+        # Auxiliary decoder stream
+        skip_decoder = [2, 4, 6, 8]
+        i = 0
+        j = 0
+        for layers in self.decoder_reconstruction:
+            if i in skip_decoder:
+                x = torch.cat((x, skip_connections_2.pop()), dim=1)
                 j += 1
             x = layers(x)
             i += 1
 
-        return x
+        return mask, x
 
 
-class Loss_Dice_CE(nn.Module):
-    def __init__(self, weight_dice=0.5):
-        super(Loss_Dice_CE, self).__init__()
+class Loss_Dice_CE_l2(nn.Module):
+    def __init__(self, weight_dice=0.5, weight_ce=0.3, weight_l2=0.2):
+        super(Loss_Dice_CE_l2, self).__init__()
+
         self.weight_dice = weight_dice
+        self.weight_ce = weight_ce
+        self.weight_l2 = weight_l2
 
-    def forward(self, logits, targets):
+        self.dice_loss = smp.losses.DiceLoss(
+            mode='multiclass',
+            classes=[0, 1, 2]
+        )
+
+    def forward(self, logits_mask, pred_img, targets_mask, targets_img):
         # Dice coefficient loss
-        dice_loss = self.dice_coefficient_loss(logits, targets)
+        dice_loss = self.dice_loss(logits_mask, targets_mask)
 
         # Cross-entropy loss
-        ce_loss = F.cross_entropy(logits, targets)
+        ce_loss = F.cross_entropy(logits_mask, targets_mask)
+
+        # L2 reconstruction loss
+        l2_loss = F.mse_loss(pred_img, targets_img)
 
         # Combine the two losses using the specified weight
-        combined_loss = self.weight_dice * dice_loss + (1 - self.weight_dice) * ce_loss
+        combined_loss = self.weight_dice * dice_loss + self.weight_ce * ce_loss + self.weight_l2 * l2_loss
 
         return combined_loss
-
-    def dice_coefficient_loss(self, logits, targets, smooth=1e-5):
-        intersection = torch.sum(logits * targets)
-        union = torch.sum(logits) + torch.sum(targets)
-        dice_coefficient = (2.0 * intersection + smooth) / (union + smooth)
-        dice_loss = 1.0 - dice_coefficient
-        return dice_loss
